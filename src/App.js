bec475ad846c198232e3607ba9394587465f1ae7
nurulamin092 <mnurulamin092@gmial.com>
2022-05-05 18:17:21 +0600

    Page Not Found components and router added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from '../src/components/Pages/Home/Home/Home'
 import About from '../src/components/Pages/About/About';
+import NotFound from '../src/components/Pages/Sheard/NotFound/NotFound';
 function App() {
   return (
     <div>
@@ -9,6 +10,7 @@ function App() {
         <Route path='/' element={<Home></Home>} />
         <Route path='/home' element={<Home></Home>} />
         <Route path='/about' element={<About></About>} />
+        <Route path='*' element={<NotFound></NotFound>} />
       </Routes>
     </div>
   );
