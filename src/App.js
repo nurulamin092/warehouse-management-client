da3a759e3cef78b8f5446b93a942960085d85933
nurulamin092 <mnurulamin092@gmial.com>
2022-05-05 19:32:53 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from '../src/components/Pages/Home/Home/Home'
+import Header from '../src/components/Pages/Shared/Header/Header';
 import About from '../src/components/Pages/About/About';
-import NotFound from '../src/components/Pages/Sheard/NotFound/NotFound';
+import NotFound from '../src/components/Pages/Shared/NotFound/NotFound';
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>} />
         <Route path='/home' element={<Home></Home>} />
--- src/components/Pages/Shared/NotFound/NotFound.js
+++ src/components/Pages/Shared/NotFound/NotFound.js
@@ -5,7 +5,6 @@ const NotFound = () => {
         <div className=''>
             <div className='w-50 mx-auto mt-5'>
                 <img src="https://media.istockphoto.com/photos/error-page-not-found-picture-id1153372686?b=1&k=20&m=1153372686&s=170667a&w=0&h=4veHHEjI5rq2qpYWMXcHSzXfrp22SMwa79DsqqiYfAw=" alt="" />
-
             </div>
         </div>
     );
