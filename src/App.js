5c96c0d4f9fc5a4c7584c8a39ff25518b88b074c
nurulamin092 <mnurulamin092@gmial.com>
2022-05-05 19:44:10 +0600

    login from add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from '../src/components/Pages/Home/Home/Home'
 import Header from '../src/components/Pages/Shared/Header/Header';
 import About from '../src/components/Pages/About/About';
+import Login from '../src/components/Pages/Login/Login';
 import NotFound from '../src/components/Pages/Shared/NotFound/NotFound';
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route path='/' element={<Home></Home>} />
         <Route path='/home' element={<Home></Home>} />
         <Route path='/about' element={<About></About>} />
+        <Route path='/login' element={<Login></Login>} />
+
         <Route path='*' element={<NotFound></NotFound>} />
       </Routes>
     </div>
