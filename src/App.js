ec775c7e02ab7d0a1ca9341bb23ed06c0acc3a01
nurulamin092 <mnurulamin092@gmial.com>
2022-05-06 09:16:42 +0600

    require auth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from '../src/components/Pages/Shared/Header/Header';
 import About from '../src/components/Pages/About/About';
 import Login from '../src/components/Pages/Login/Login/Login';
 import Register from '../src/components/Pages/Login/Register/Register';
+import RequireAuth from '../src/components/Pages/Login/RequireAuth/RequireAuth';
 import NotFound from '../src/components/Pages/Shared/NotFound/NotFound';
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>} />
         <Route path='/home' element={<Home></Home>} />
-        <Route path='/about' element={<About></About>} />
+        <Route path='/about' element={<RequireAuth><About></About></RequireAuth>} />
         <Route path='/login' element={<Login></Login>} />
         <Route path='/register' element={<Register></Register>} />
         <Route path='*' element={<NotFound></NotFound>} />
