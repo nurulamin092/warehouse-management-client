dedde8e46f4548a9417375371265e750cb90b362
nurulamin092 <mnurulamin092@gmial.com>
2022-05-06 08:31:06 +0600

    firebase email and password register done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Home from '../src/components/Pages/Home/Home/Home'
 import Header from '../src/components/Pages/Shared/Header/Header';
 import About from '../src/components/Pages/About/About';
-import Login from '../src/components/Pages/Login/Login';
+import Login from '../src/components/Pages/Login/Login/Login';
+import Register from '../src/components/Pages/Login/Register/Register';
 import NotFound from '../src/components/Pages/Shared/NotFound/NotFound';
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path='/home' element={<Home></Home>} />
         <Route path='/about' element={<About></About>} />
         <Route path='/login' element={<Login></Login>} />
-
+        <Route path='/register' element={<Register></Register>} />
         <Route path='*' element={<NotFound></NotFound>} />
       </Routes>
     </div>
---
+++ src/components/Pages/Login/Register/Register.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../../firebase.init';
+
+const Register = () => {
+    const [agree, setAgree] = useState(false);
+    const [displayName, setDisplayName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const [
+        createUserWithEmailAndPassword,
+
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [updateProfile] = useUpdateProfile(auth);
+    const handleNameBlur = (e) => {
+        setDisplayName(e.target.value);
+    }
+    const handleEmailBlur = (e) => {
+        setEmail(e.target.value);
+    }
+    const handlePasswordBlur = (e) => {
+        setPassword(e.target.value);
+    }
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        await createUserWithEmailAndPassword(email, password)
+        await updateProfile(displayName);
+        e.target.reset();
+        navigate('/home');
+    }
+    const navigateLogin = (e) => {
+        navigate('/login');
+    }
+    return (
+        <div>
+            <div className='w-50 mx-auto mt-2'>
+                <Form onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Control onBlur={handleNameBlur} type="text" placeholder="Enter Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className={`ps-2 ${agree ? '' : 'text-danger'} mb-3`} controlId="formBasicCheckbox">
+                        <Form.Check onClick={() => setAgree(!agree)} type="checkbox" label="Accept Terms and Condition" />
+                    </Form.Group>
+                    <Button
+                        disabled={!agree}
+                        variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+                <p className='d-flex'><span className='mx-2'>Already Have an Account?</span> <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+
+            </div>
+        </div>
+    );
+};
+
+export default Register;
