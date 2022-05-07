38edfac26522b5f60218bd6456bda486221bc69b
nurulamin092 <mnurulamin092@gmial.com>
2022-05-07 19:53:51 +0600

    add fake product

---
+++ src/components/Pages/Home/Product/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Product = ({ product }) => {
+    const { id, name, price, img, description } = product;
+    return (
+        <div>
+            <img height={300} src={img} alt="" />
+            <h1>{name}</h1>
+            <h2>{price}</h2>
+            <p>{description}</p>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Pages/Home/Products/Products.js
+++ src/components/Pages/Home/Products/Products.js
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './products.css'
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('product.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [])
     return (
-        <div>
+        <div id='products' className='container'>
+            <h2 className='text-primary text-center mt-5'>Our Products:{products.length}</h2>
+            <div className='products-container'>
+                {
+                    products.map(product => <Product key={product.id}
+                        product={product}>
 
+                    </Product>)
+                }
+            </div>
         </div>
     );
 };
