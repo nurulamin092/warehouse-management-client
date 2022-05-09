d6e40e5f39066f0f66bec203292b1fbb4c965efd
nurulamin092 <mnurulamin092@gmial.com>
2022-05-09 23:47:37 +0600

    use hook server link

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 const useProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        const localhostUrl = `http://localhost:5000/products`;
-        // const serverUrl = `https://serene-brook-28678.herokuapp.com/products`;
-        fetch(localhostUrl)
+        //const localhostUrl = `http://localhost:5000/products`;
+        const serverUrl = `https://serene-brook-28678.herokuapp.com/products`;
+        fetch(serverUrl)
             .then(res => res.json())
             .then(data => setProducts(data));
     }, []);
